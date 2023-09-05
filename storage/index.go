e09eabda50cd2343b34ba1e7522adde9be8bf758
JT Olio <jt@olio.lol>
2023-09-05 12:43:57 -0400

    title

--- storage/index.go
+++ storage/index.go
@@ -10,6 +10,7 @@ var indexHTML = template.Must(template.New("index").Parse(`<!doctype html>
 <meta charset=utf-8>
 <meta name=viewport content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+<title>Work Presence</title>
 </head>
 <body onload="main();">
   <ul id="bucket-content"></ul>
